4e2debfdeaee464246b079440adebacaed005e89
gbaup01@gmail.com <gbaup01@gmail.com@fe754ac9-803c-2ff0-c19c-5cfec0cf607e>
2012-10-24 13:35:18 +0000

    1

--- zogci/resource.h
+++ zogci/resource.h
@@ -50,6 +50,7 @@
 #define IDC_BUTTON_TAIBNET              1074
 #define IDC_BUTTON_AFU                  1075
 #define IDC_BUTTON_bettaman             1076
+#define IDC_BUTTON_SISHOU               1077
 
 // Next default values for new objects
 // 
@@ -57,7 +58,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        132
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1077
+#define _APS_NEXT_CONTROL_VALUE         1078
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
