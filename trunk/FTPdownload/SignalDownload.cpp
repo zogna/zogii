d574dd706c8d526545791f4b5f7acbde65e0fd8c
gbaup01@gmail.com <gbaup01@gmail.com@fe754ac9-803c-2ff0-c19c-5cfec0cf607e>
2012-11-22 12:46:01 +0000

    12

--- trunk/FTPdownload/SignalDownload.cpp
+++ trunk/FTPdownload/SignalDownload.cpp
@@ -1,5 +1,6 @@
 #include "SignalDownload.h"
 
+#define LOWSPEEDLIMIT 256
 
 //扩展定义
 #define SD_curl_easy_setopt_EXT(A,B,C)			\
@@ -13,7 +14,29 @@
 		A=NULL;								\
 		return false;							\
 	}
-	
+
+//扩展定义
+#define SD_curl_easy_setopt_EXT_NOFILE(A,B,C)	\
+	res=curl_easy_setopt(A,B,C);				\
+	if(CURLE_OK != res)  /* we failed */		\
+	{											\
+		fprintf(stderr, "ERROR:%s,%d,curl told us %s\n", __FILE__,__LINE__,curl_easy_strerror(res));	\
+		curl_easy_cleanup(A);					\
+		A=NULL;									\
+		return false;							\
+	}
+
+//扩展定义
+#define SD_curl_easy_getinfo_EXT_NOFILE(A,B,C)	\
+	res=curl_easy_getinfo(A,B,C);				\
+	if(CURLE_OK != res)  /* we failed */		\
+	{											\
+		fprintf(stderr, "ERROR:%s,%d,curl told us %s\n", __FILE__,__LINE__,curl_easy_strerror(res));	\
+		curl_easy_cleanup(A);					\
+		A=NULL;									\
+		return false;							\
+	}
+
 //下载并写到文件中 回调函数
 /*
 typedef size_t (*curl_write_callback)(char *buffer,
@@ -56,7 +79,7 @@ int SD_ProgressFunc(void *clientp,
 //timeout	设定下载超时时限
 //输出		下载成功或失败
 //执行下载
-bool SignalDownload::Download(char *url,TCHAR *filename,long timeout=10,long startsize=0)
+bool SignalDownload::HTTPDownload(char *url,TCHAR *filename,long timeout=10,long startsize=0)
 {
 	completesize=0;
 	totalsize=0;
@@ -94,6 +117,11 @@ bool SignalDownload::Download(char *url,TCHAR *filename,long timeout=10,long sta
 	SD_curl_easy_setopt_EXT(curl, CURLOPT_TIMEOUT, timeout);
 	//设置URL地址错误 重连N次后推出
 	SD_curl_easy_setopt_EXT(curl, CURLOPT_CONNECTTIMEOUT, timeout);
+	//设置最低速度。为了中途拔网线
+	SD_curl_easy_setopt_EXT(curl, CURLOPT_LOW_SPEED_LIMIT, LOWSPEEDLIMIT);
+	SD_curl_easy_setopt_EXT(curl, CURLOPT_LOW_SPEED_TIME, timeout);
+
+	
 	//开始执行
 	res = curl_easy_perform(curl);
 	if(CURLE_OK != res)  /* we failed */				
@@ -114,4 +142,157 @@ bool SignalDownload::Download(char *url,TCHAR *filename,long timeout=10,long sta
 	curl=NULL;
 
 	return true;
+}
+//实时上传回调
+size_t SD_ReadFunc(void *ptr, size_t size, size_t nmemb, FILE *stream)
+{
+	//fprintf(stderr, "*** We read %d bytes from file\n", nmemb);
+	return fread(ptr, size, nmemb, stream);
+}
+
+//url		URL地址 
+//userpsw	用户名和密码 "user:psw"
+//port		FTP端口号
+//filename	文件路径
+//timeout	设定上传超时时限
+//输出		上传成功或失败
+//执行上传
+bool SignalDownload::FTPUpload(char *url,char *userpsw,long port,TCHAR *filename,long timeout=10)
+{
+	curl_off_t fsize;
+
+	curl=NULL;
+	/* get a curl handle */
+	curl = curl_easy_init();
+	if(NULL == curl)
+		return false;
+
+	/* get the file size of the local file */
+	/* get a FILE * of the same file */
+	outfile=NULL;
+	outfile = _tfopen(filename, _T("rb"));
+	if(NULL== outfile)
+	{
+		curl_easy_cleanup(curl);
+		curl=NULL;
+		return false;
+	}
+
+	fseek(outfile,0,SEEK_END);
+	fsize =	ftell(outfile);
+	fseek(outfile,0,SEEK_SET);
+
+	//printf("Local file size: %" CURL_FORMAT_CURL_OFF_T " bytes.\n", fsize);
+
+	/* enable uploading */
+	SD_curl_easy_setopt_EXT(curl, CURLOPT_UPLOAD, 1L);
+
+	/* specify target */
+	SD_curl_easy_setopt_EXT(curl,CURLOPT_URL, url);
+	SD_curl_easy_setopt_EXT(curl,CURLOPT_USERPWD, userpsw);
+	SD_curl_easy_setopt_EXT(curl,CURLOPT_PORT, port);
+
+	//设置下载速度=0时 N次退出
+	SD_curl_easy_setopt_EXT(curl, CURLOPT_TIMEOUT, timeout);
+	//设置URL地址错误 重连N次后推出
+	SD_curl_easy_setopt_EXT(curl, CURLOPT_CONNECTTIMEOUT, timeout);
+	SD_curl_easy_setopt_EXT(curl, CURLOPT_FTP_RESPONSE_TIMEOUT, timeout);
+	//设置最低速度。为了中途拔网线
+	SD_curl_easy_setopt_EXT(curl, CURLOPT_LOW_SPEED_LIMIT, LOWSPEEDLIMIT);
+	SD_curl_easy_setopt_EXT(curl, CURLOPT_LOW_SPEED_TIME, timeout);
+
+	/* pass in that last of FTP commands to run after the transfer */
+	SD_curl_easy_setopt_EXT(curl, CURLOPT_FTPPORT, "-"); /* disable passive mode */
+	SD_curl_easy_setopt_EXT(curl, CURLOPT_FTP_CREATE_MISSING_DIRS, 1L);
+
+	SD_curl_easy_setopt_EXT(curl, CURLOPT_VERBOSE, 1L);
+
+	/* we want to use our own read function */
+	SD_curl_easy_setopt_EXT(curl, CURLOPT_READFUNCTION, SD_ReadFunc);
+
+	/* now specify which file to upload */
+	SD_curl_easy_setopt_EXT(curl, CURLOPT_READDATA, outfile);
+
+	/* Set the size of the file to upload (optional).  If you give a *_LARGE
+	option you MUST make sure that the type of the passed-in argument is a
+	curl_off_t. If you use CURLOPT_INFILESIZE (without _LARGE) you must
+	make sure that to pass in a type 'long' argument. */
+	SD_curl_easy_setopt_EXT(curl, CURLOPT_INFILESIZE_LARGE,(curl_off_t)fsize);
+
+	SD_curl_easy_setopt_EXT(curl, CURLOPT_INFILESIZE,(curl_off_t)fsize);
+
+	/* Now run off and do what you've been told! */
+	res = curl_easy_perform(curl);
+	/* Check for errors */
+	if(CURLE_OK != res)  /* we failed */				
+	{												
+		fprintf(stderr, "curl result %s\n",curl_easy_strerror(res));	
+		fclose(outfile);
+		outfile=NULL;
+		curl_easy_cleanup(curl);
+		curl=NULL;
+		return false;							
+	}	
+
+	//关闭文件
+	fclose(outfile);
+	outfile=NULL;
+
+	curl_easy_cleanup(curl);
+	curl=NULL;
+
+	return true;
+}
+
+size_t SD_HeaderFunc(void *ptr, size_t size, size_t nmemb, void *data)
+{
+  (void)ptr;
+  (void)data;
+  /* we are not interested in the headers itself,
+     so we only return the size we would have saved ... */
+  return (size_t)(size * nmemb);
+}
+
+bool SignalDownload::FTPFileinfo(char *url,char *userpsw,long port, time_t *filetime,double *filesize,long timeout=10)
+{
+	curl=NULL;
+	/* get a curl handle */
+	curl = curl_easy_init();
+	if(NULL == curl)
+		return false;
+	
+	SD_curl_easy_setopt_EXT_NOFILE(curl,CURLOPT_URL,url);
+	SD_curl_easy_setopt_EXT_NOFILE(curl,CURLOPT_USERPWD,userpsw);
+	SD_curl_easy_setopt_EXT_NOFILE(curl,CURLOPT_PORT, port);
+	/* No download if the file */
+	SD_curl_easy_setopt_EXT_NOFILE(curl, CURLOPT_NOBODY, 1L);
+	/* Ask for filetime */
+	SD_curl_easy_setopt_EXT_NOFILE(curl, CURLOPT_FILETIME, 1L);
+	/* No header output: TODO 14.1 http-style HEAD output for ftp */
+	SD_curl_easy_setopt_EXT_NOFILE(curl, CURLOPT_HEADERFUNCTION, SD_HeaderFunc);
+	SD_curl_easy_setopt_EXT_NOFILE(curl, CURLOPT_HEADER, 0L);
+	/* Switch on full protocol/debug output */
+	/* curl_easy_setopt(curl, CURLOPT_VERBOSE, 1L); */
+
+	res = curl_easy_perform(curl);
+
+	if(CURLE_OK != res) 
+	{
+		fprintf(stderr, "curl result %s\n",curl_easy_strerror(res));	
+		curl_easy_cleanup(curl);
+		curl=NULL;
+		return false;	
+	}
+
+	SD_curl_easy_getinfo_EXT_NOFILE(curl, CURLINFO_FILETIME, filetime);
+	if(*filetime)
+		printf("filetime: %s",ctime(filetime));
+
+	SD_curl_easy_getinfo_EXT_NOFILE(curl, CURLINFO_CONTENT_LENGTH_DOWNLOAD, filesize);
+	if(*filesize)
+		printf("filesize: %0.0f bytes\n", *filesize);
+
+	curl_easy_cleanup(curl);
+	curl=NULL;
+	return	true;
 }
